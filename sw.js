050f89b061410b7d4248488d353c12d78c80d7c9
Makaron44 <99068890+Makaron44@users.noreply.github.com>
2025-09-14 20:16:11 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME='mini-lotto-cache-v7';
+const CACHE_NAME='mini-lotto-cache-v8';
 const PRECACHE_URLS=['./','./index.html','./results.html','./settings.html','./css/styles.css','./js/db.js','./js/pwa.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(PRECACHE_URLS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))));self.clients.claim();});
